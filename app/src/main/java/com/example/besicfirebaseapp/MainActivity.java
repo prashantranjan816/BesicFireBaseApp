5812d5ef412069586d7751fda39b03eb1d644df6
Prashant Ranjan <prashantranjan@prashants-macbook-air.local>
2019-04-19 08:54:35 +0530

    First Commit with add a notification button.

--- app/src/main/java/com/example/besicfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/besicfirebaseapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.besicfirebaseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void NotificationTest(View view) {
+        Toast.makeText(this, "FCM NOtification", Toast.LENGTH_SHORT).show();
     }
 }
